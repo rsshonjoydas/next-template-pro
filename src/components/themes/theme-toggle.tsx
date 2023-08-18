45377425239ce63bb2fb8df5ed04b3c37511ee4b
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-08-18 18:47:46 +0600

    ci: dropdown 3 with color

--- src/components/themes/theme-toggle.tsx
+++ src/components/themes/theme-toggle.tsx
@@ -18,33 +18,44 @@ const ThemeToggle = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant='ghost' className='px-0 w-9'>
-          <Icons.Sun className='w-6 h-6 transition-all scale-100 rotate-0 stroke-primary dark:-rotate-90 dark:scale-0' />
-          <Icons.Moon className='absolute w-6 h-6 transition-all scale-0 rotate-90 fill-primary dark:rotate-0 dark:scale-100' />
+        <Button variant='ghost' className='px-2'>
+          <Icons.Sun className='w-6 h-6 transition-all scale-100 rotate-0 hover:text-slate-900 dark:-rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100 stroke-sky-500' />
+          <Icons.Moon className='absolute w-6 h-6 transition-all scale-0 rotate-90 hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100 fill-sky-500' />
           <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
+      <DropdownMenuContent align='end' forceMount>
         <DropdownMenuItem
           onClick={() => setTheme('light')}
           className={`${theme === 'light' ? 'bg-accent' : ''}`}
         >
-          <Icons.Sun className='w-5 h-5 mr-2 stroke-primary' />
-          Light
+          <Icons.Sun
+            className={`w-5 h-5 mr-2 ${theme === 'light' ? 'stroke-sky-500' : 'stroke-slate-400'}`}
+          />
+          <span className={`${theme === 'light' ? 'text-sky-500' : null}`}>Light</span>
         </DropdownMenuItem>
         <DropdownMenuItem
           onClick={() => setTheme('dark')}
           className={`${theme === 'dark' ? 'bg-accent' : ''}`}
         >
-          <Icons.Moon className='w-5 h-5 mr-2 fill-primary' />
-          Dark
+          <Icons.Moon
+            className={`w-5 h-5 mr-2 ${theme === 'dark' ? 'fill-sky-500' : 'fill-slate-400'}`}
+          />
+
+          <span className={`${theme === 'dark' ? 'text-sky-500' : null}`}>Dark</span>
         </DropdownMenuItem>
         <DropdownMenuItem
           onClick={() => setTheme('system')}
           className={`${theme === 'system' || theme === null ? 'bg-accent' : ''}`}
         >
-          <Icons.System className='w-5 h-5 mr-2 stroke-primary' />
-          System
+          <Icons.System
+            className={`${
+              theme === 'system' || theme === null ? 'stroke-sky-500' : 'stroke-slate-400'
+            }`}
+          />
+          <span className={`${theme === 'system' || theme === null ? 'text-sky-500' : ''}`}>
+            System
+          </span>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
