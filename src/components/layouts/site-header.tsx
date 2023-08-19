33ea26153251f783ea64706bd6ff661dcf0e8cfb
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-08-19 10:49:19 +0600

    ci: common dropdown nav components

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { mainNav } from '@/config/nav';
+import { menuConfig } from '@/config/nav';
 import { siteConfig } from '@/config/site';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
@@ -15,18 +15,18 @@ const SiteHeader = () => (
   <header className='sticky top-0 z-50 w-full border-b h-14 supports-backdrop-blur:bg-background/60 bg-background/95 backdrop-blur'>
     <div className='container px-4 mx-auto sm:px-6'>
       <div className='flex items-center justify-between lg:justify-end'>
-        <div className='md:hidden'>
+        <div className='lg:hidden'>
           <div className='inline-flex items-center justify-center p-3 cursor-pointer'>
-            <MobileNav items={mainNav} />
+            <MobileNav mainNavItems={menuConfig.mainNav} />
           </div>
         </div>
 
-        <div className='justify-start hidden md:flex md:-mt-2 lg:w-0 lg:flex-1'>
+        <div className='justify-start hidden lg:flex lg:w-0 lg:flex-1'>
           <Logo />
         </div>
 
         <nav className='justify-end hidden p-3 md:flex lg:flex'>
-          <MainNav items={mainNav} />
+          <MainNav items={menuConfig.mainNav} />
         </nav>
 
         <nav className='flex items-center'>
--- src/config/nav.ts
+++ src/config/nav.ts
@@ -1,4 +1,6 @@
+import { slugify } from '@/lib/utils';
 import { MainNavItem } from '@/types/nav';
+import { productCategories } from './products';
 
 export const mainNav: MainNavItem[] = [
   {
@@ -29,3 +31,54 @@ export const mainNav: MainNavItem[] = [
     disabled: true,
   },
 ];
+
+export const menuConfig = {
+  name: 'Shonjoy',
+  description: 'An open source e-commerce Shonjoy build with everything new in Next.js 13.',
+  url: 'https://skateshop.sadmn.com',
+  ogImage: 'https://skateshop.sadmn.com/opengraph-image.png',
+  mainNav: [
+    {
+      title: 'Lobby',
+      items: [
+        {
+          title: 'Products',
+          href: '/products',
+          description: 'All the products we have to offer.',
+          items: [],
+        },
+        {
+          title: 'Build a Board',
+          href: '/build-a-board',
+          description: 'Build your own custom skateboard.',
+          items: [],
+        },
+        {
+          title: 'Blog',
+          href: '/blog',
+          description: 'Read our latest blog posts.',
+          items: [],
+        },
+      ],
+    },
+    ...productCategories.map((category) => ({
+      title: category.title,
+      items: [
+        {
+          title: 'All',
+          href: `/categories/${slugify(category.title)}`,
+          description: `All ${category.title}.`,
+          items: [],
+        },
+        ...category.subcategories.map((subcategory) => ({
+          title: subcategory.title,
+          href: `/categories/${slugify(category.title)}/${subcategory.slug}`,
+          description: subcategory.description,
+          items: [],
+        })),
+      ],
+    })),
+  ] satisfies MainNavItem[],
+};
+
+export type MenuConfig = typeof menuConfig;
