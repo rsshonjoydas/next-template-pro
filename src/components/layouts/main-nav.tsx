866eabac019f4e441d0e8c2b7355ce2ee2cfe261
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-08-19 23:05:33 +0600

    ci: dropdown nav 1

--- src/components/layouts/main-nav.tsx
+++ src/components/layouts/main-nav.tsx
@@ -1,20 +1,113 @@
 'use client';
 
 import { MainNavItem } from '@/types/nav';
-import NavItem from './nav-item';
+import Link from 'next/link';
+import * as React from 'react';
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import { siteConfig } from '@/config/site';
+import { cn } from '@/lib/utils';
 
 interface MobileNavProps {
   items: MainNavItem[];
 }
 
+const ListItem = React.forwardRef<React.ElementRef<'a'>, React.ComponentPropsWithoutRef<'a'>>(
+  ({ className, title, children, href, ...props }, ref) => (
+    <li>
+      <NavigationMenuLink asChild>
+        <Link
+          ref={ref}
+          href={String(href)}
+          className={cn(
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+            className
+          )}
+          {...props}
+        >
+          <div className='text-sm font-medium leading-none'>{title}</div>
+          <p className='text-sm leading-snug line-clamp-2 text-muted-foreground'>{children}</p>
+        </Link>
+      </NavigationMenuLink>
+    </li>
+  )
+);
+ListItem.displayName = 'ListItem';
+
 const MainNav = ({ items }: MobileNavProps) => (
-  <nav className='container px-4 mx-auto'>
-    <div className='flex items-center justify-between lg:justify-end'>
-      <div className='justify-end hidden md:flex lg:flex'>
-        <NavItem items={items} className='hidden gap-6 md:flex' />
-      </div>
-    </div>
-  </nav>
+  <div className='hidden gap-6 lg:flex'>
+    <NavigationMenu>
+      <NavigationMenuList>
+        {items?.[0]?.items ? (
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className='h-auto'>{items[0].title}</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className='grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
+                <li className='row-span-3'>
+                  <NavigationMenuLink asChild>
+                    <a
+                      aria-label='Home'
+                      className='flex flex-col justify-end w-full h-full p-6 no-underline rounded-md outline-none select-none bg-gradient-to-b from-muted/50 to-muted focus:shadow-md'
+                      href='/'
+                    >
+                      {/* <Icons.logo className='w-6 h-6' aria-hidden='true' /> */}
+                      <div className='mt-4 mb-2 text-lg font-medium'>{siteConfig.name}</div>
+                      <p className='text-sm leading-tight text-muted-foreground'>
+                        {siteConfig.description}
+                      </p>
+                    </a>
+                  </NavigationMenuLink>
+                </li>
+                {items[0].items.map((listItem) => (
+                  <ListItem key={listItem.title} title={listItem.title} href={listItem.href}>
+                    {listItem.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        ) : null}
+        {items
+          ?.filter((navItem) => navItem.title !== items[0]?.title)
+          .map((item) =>
+            item?.items ? (
+              <NavigationMenuItem key={item.title}>
+                <NavigationMenuTrigger className='h-auto capitalize'>
+                  {item.title}
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]'>
+                    {item.items.map((menuItem) => (
+                      <ListItem key={menuItem.title} title={menuItem.title} href={menuItem.href}>
+                        {menuItem.description}
+                      </ListItem>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            ) : (
+              item.href && (
+                <NavigationMenuItem key={item.title}>
+                  <Link href={item.href} legacyBehavior passHref>
+                    <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), 'h-auto')}>
+                      {item.title}
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              )
+            )
+          )}
+      </NavigationMenuList>
+    </NavigationMenu>
+  </div>
 );
 
 export default MainNav;
--- src/components/layouts/mobile-nav.tsx
+++ src/components/layouts/mobile-nav.tsx
@@ -1,46 +1,115 @@
-import { siteConfig } from '@/config/site';
-import useModal from '@/hooks/use-modal';
-import { MainNavItem } from '@/types/nav';
-import Link from 'next/link';
-import Icons from '../icons';
-import { Button } from '../ui/button';
-import Modal from '../ui/modal';
-import NavItem from './nav-item';
+/* eslint-disable no-shadow */
+/* eslint-disable react/no-array-index-key */
 
-interface MobileNavProps {
-  items: MainNavItem[];
+'use client';
+
+import type { MainNavItem } from '@/types/nav';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import * as React from 'react';
+
+import Icons from '@/components/icons';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+import { Button } from '@/components/ui/button';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { siteConfig } from '@/config/site';
+import { cn } from '@/lib/utils';
+
+interface MobileLinkProps {
+  children?: React.ReactNode;
+  href: string;
+  disabled?: boolean;
+  pathname: string;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const MobileNav = ({ items }: MobileNavProps) => {
-  const { modalOpen, close, open } = useModal();
+interface MobileNavProps {
+  mainNavItems?: MainNavItem[];
+}
+
+function MobileLink({ children, href, disabled, pathname, setIsOpen }: MobileLinkProps) {
+  return (
+    <Link
+      href={href}
+      className={cn(
+        'text-foreground/70 transition-colors hover:text-foreground',
+        pathname === href && 'text-foreground',
+        disabled && 'pointer-events-none opacity-60'
+      )}
+      onClick={() => setIsOpen(false)}
+    >
+      {children}
+    </Link>
+  );
+}
+
+const MobileNav = ({ mainNavItems }: MobileNavProps) => {
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = React.useState(false);
 
   return (
-    <>
-      <div className='relative flex items-center justify-center'>
-        <Button variant='ghost' onClick={open} className='px-2'>
-          <Icons.Menu className='flex-none w-6 h-6 stroke-foreground' />
-        </Button>
-      </div>
-      {modalOpen && (
-        <Modal
-          modalOpen={modalOpen}
-          text='drawer'
-          type='drawer'
-          handleClose={close}
-          className='h-full border-r-2 shadow-lg bg-background'
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger asChild>
+        <Button
+          variant='ghost'
+          className='px-0 mr-2 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 lg:hidden'
         >
-          <Button variant='outline' type='button' onClick={close} className='mt-2 ml-44'>
-            <Icons.Close className='flex-none w-4 h-4 -m-2 stroke-foreground' />
-          </Button>
-          <div className='flex flex-col items-start justify-center p-2 -mt-6 text-lg'>
-            <Link href='/' className='flex items-center mr-6 space-x-2'>
-              <span className='font-bold'>{siteConfig.name}</span>
-            </Link>
-            <NavItem items={items} className='grid grid-flow-row mt-8 text-sm auto-rows-ma' />
+          <Icons.Menu className='flex-none w-6 h-6 stroke-foreground' />
+          <span className='sr-only'>Toggle Menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side='left' className='w-1/2 pl-1 pr-0'>
+        <div className='px-7'>
+          <Link
+            aria-label='Home'
+            href='/'
+            className='flex items-center'
+            onClick={() => setIsOpen(false)}
+          >
+            {/* <Icons.logo className='w-4 h-4 mr-2' aria-hidden='true' /> */}
+            <span className='font-bold'>{siteConfig.name}</span>
+          </Link>
+        </div>
+        <ScrollArea className='my-4 h-[calc(100vh-8rem)] pb-10 pl-6'>
+          <div className='pl-1 pr-7'>
+            <Accordion type='single' collapsible className='w-full'>
+              {mainNavItems?.map((item, index) => (
+                <AccordionItem value={item.title} key={index}>
+                  <AccordionTrigger className='text-sm capitalize'>{item.title}</AccordionTrigger>
+                  <AccordionContent>
+                    <div className='flex flex-col space-y-2'>
+                      {item.items?.map((subItem, index) =>
+                        subItem.href ? (
+                          <MobileLink
+                            key={index}
+                            href={String(subItem.href)}
+                            pathname={pathname}
+                            setIsOpen={setIsOpen}
+                            disabled={subItem.disabled}
+                          >
+                            {subItem.title}
+                          </MobileLink>
+                        ) : (
+                          <div key={index} className='transition-colors text-foreground/70'>
+                            {item.title}
+                          </div>
+                        )
+                      )}
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+              ))}
+            </Accordion>
           </div>
-        </Modal>
-      )}
-    </>
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
   );
 };
 
