3fe11bf0fad2d4c6d55c200ee541e78868bf2d05
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-08-25 10:01:41 +0600

    fix: theme toggle icons color

--- src/components/themes/theme-toggle.tsx
+++ src/components/themes/theme-toggle.tsx
@@ -19,8 +19,8 @@ const ThemeToggle = () => {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant='ghost' className='px-0 w-9'>
-          <Icons.Sun className='w-6 h-6 transition-all scale-100 rotate-0 stroke-primary dark:-rotate-90 dark:scale-0' />
-          <Icons.Moon className='absolute w-6 h-6 transition-all scale-0 rotate-90 fill-primary dark:rotate-0 dark:scale-100' />
+          <Icons.Sun className='w-6 h-6 transition-all scale-100 rotate-0 stroke-foreground dark:-rotate-90 dark:scale-0' />
+          <Icons.Moon className='absolute w-6 h-6 transition-all scale-0 rotate-90 fill-foreground dark:rotate-0 dark:scale-100' />
           <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
@@ -29,14 +29,14 @@ const ThemeToggle = () => {
           onClick={() => setTheme('light')}
           className={`${theme === 'light' ? 'bg-accent text-foreground/80' : ''}`}
         >
-          <Icons.Sun className='w-5 h-5 mr-2 stroke-primary' />
+          <Icons.Sun className='w-5 h-5 mr-2 stroke-foreground' />
           Light
         </DropdownMenuItem>
         <DropdownMenuItem
           onClick={() => setTheme('dark')}
           className={`${theme === 'dark' ? 'bg-accent text-foreground/80' : ''}`}
         >
-          <Icons.Moon className='w-5 h-5 mr-2 fill-primary' />
+          <Icons.Moon className='w-5 h-5 mr-2 fill-foreground' />
           Dark
         </DropdownMenuItem>
         <DropdownMenuItem
@@ -45,7 +45,7 @@ const ThemeToggle = () => {
             theme === 'system' || theme === null ? 'bg-accent text-foreground/80' : ''
           }`}
         >
-          <Icons.System className='w-5 h-5 mr-2 stroke-primary' />
+          <Icons.System className='w-5 h-5 mr-2 stroke-foreground' />
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
